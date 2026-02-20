12ccdd41b3464ffbbecac0245b4c243737526d5e
manoday <manodayahire786@gmail.com>
2026-02-20 19:29:42 +0530

    fix

--- VM/linux/setup.sh
+++ VM/linux/setup.sh
@@ -1,10 +1,12 @@
 echo "================================================================"
 echo "Installing Signoz VM agent"
 echo "================================================================"
+
+echo "#########  COPY/ Create config.yaml to the otelcol-contrib DIR  ########## "
+
 echo " Downloading otel-collector tar.gz for your amd architecture"
 sudo wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.143.1/otelcol-contrib_0.143.1_linux_amd64.tar.gz
-echo " Extracting otel-collector tar.gz to the otelcol-contrib folder"
-
+echo "Extracting otel-collector tar.gz to the otelcol-contrib folder"
 sudo mkdir otelcol-contrib && tar xvzf otelcol-contrib_0.143.1_linux_amd64.tar.gz -C otelcol-contrib
 sudo cp config.yaml otelcol-contrib/
 echo "Runnig otel contrib in Background"
