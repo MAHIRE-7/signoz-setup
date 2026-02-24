464716fdac2e6dd9ec3b4e3c9bab2705e2c5f519
manoday <manodayahire786@gmail.com>
2026-02-24 11:00:26 +0530

    fix: dir

--- signoz/frontend/src/hooks/queryBuilder/useQueryBuilder.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { QueryBuilderContext } from 'providers/QueryBuilder';
-import { QueryBuilderContextType } from 'types/common/queryBuilder';
-
-export function useQueryBuilder(): QueryBuilderContextType {
-	return useContext(QueryBuilderContext);
-}
